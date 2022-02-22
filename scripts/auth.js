ae2b3f171791f15f720ce478c1b53ca947b43412
arthur <you@example.com>
2022-02-22 20:44:12 +0200

    authentication

---
+++ scripts/auth.js
@@ -0,0 +1,58 @@
+/*
+ @role register user
+*/
+function addUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('Pass').value;
+    const first_name = document.getElementById('Uname').value;
+    // const last_name = document.getElementById('last_name').value;
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            // alert("User created successfully")
+            saveUserProfile({ first_name, email })
+                // window.location.href = "./index.html";
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Error: " + errorMessage)
+        });
+}
+/*
+ @role register user
+*/
+function loginUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('Pass').value;
+    auth.signInWithEmailAndPassword(email, password).then(userCredential => {
+            const user = userCredential.user;
+            localStorage.setItem('user', JSON.stringify(user))
+            window.location.href = "../dasboard/dashboard.html";
+        })
+        .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage || "An error occurred");
+        });
+}
+
+/*
+ @role save user profile
+*/
+function saveUserProfile({ first_name, last_name, email }) {
+    db.collection('users').doc().set({
+        first_name,
+        email,
+        created_at: new Date()
+    }).then(() => {
+        alert("You successfully created account");
+        window.location.href = "../sign-in/signin.html";
+    }).catch((error) => {
+        alert(error?.message || 'An error occurred');
+    })
+}
+
+/*
+ @role get profile
+*/
---
+++ scripts/firebase.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAb3626tLmGRRZOULgaS9TLdJ27Z9l29JA",
+    authDomain: "web-app-e5df4.firebaseapp.com",
+    projectId: "web-app-e5df4",
+    storageBucket: "web-app-e5df4.appspot.com",
+    messagingSenderId: "1032356009472",
+    appId: "1:1032356009472:web:ef462564996bb0133f2ae5",
+    measurementId: "G-Q7LS3FK7HG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//make aut and firestore references
+// enable firebase auth service
+const auth = firebase.auth();
+// enable firebase firestore service
+const db = firebase.firestore();
+//   db.settings({timeStampInShots: true});
