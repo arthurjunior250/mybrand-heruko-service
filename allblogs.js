d34060a4de571b56e181034a6b7e5455f781af05
arthur <you@example.com>
2022-03-22 15:29:16 +0200

    feature(blog):added blog

---
+++ allblogs.js
@@ -0,0 +1,39 @@
+
+const getBlogs = async() => {
+    let result = [];
+    fetch('https://my-brand-endpoints.herokuapp.com/api/v1/blog', {
+            method: "GET",
+        })
+        .then(response => response.json())
+        .then(json => {
+            result = json.data
+            console.log(json.data)
+
+            result?.length ?
+                document.querySelector("#blog__card").innerHTML = result.map((res) =>
+                    `
+      <div class="blog-sect">
+               <img src=${res?.image} alt="blog">
+        <a href="./blogs/blog.html" onclick="getBlogId('${res._id}')">
+        <h1> ${res?.title}</h1>
+        </a>
+       <p> ${res?.description?.slice(1,200)}</p>
+
+      <div class="emoji1">
+           <img src="../images/comment.png" alt="" srcset="">
+             <img src="../images/like.png" alt="" srcset="">
+         </div>
+          </div>
+      `
+                ).join("") :
+                document.querySelector("#blog__card").innerHTML = `<img style="width:150%;"src="https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif" alt="spinner">`
+        })
+        .catch(err => console.log(err));
+}
+getBlogs();
+
+
+
+const getBlogIdi = (key) => {
+    location.href = `../blogs/blog.html?id=${key}`;
+}
---
+++ createblogs.js
@@ -0,0 +1,32 @@
+
+const image = document.getElementById("imaging");
+const title = document.getElementById("title-name");
+const description = document.getElementById("body-message");
+const btnContact= document.getElementById("create");
+
+btnContact.addEventListener("click", (e) => {
+  e.preventDefault();
+const token =localStorage.getItem('token');
+  fetch(
+    "https://my-brand-endpoints.herokuapp.com/api/v1/blog",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        image: image.value,
+        title: title.value,
+        description:description.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": 'bearer '+token
+      },
+    }
+  )
+    .then((response) => {
+
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
