0988e227e72302400ea4bee8a10b81d368860480
arthur <you@example.com>
2022-04-11 11:42:40 +0200

    userDash

---
+++ userPage/userDash/userBlogs.js
@@ -0,0 +1,32 @@
+const getBlogs = async() => {
+    let result = [];
+    fetch('https://my-brand-endpoints.herokuapp.com/api/v1/blog', {
+            method: "GET",
+        })
+        .then(response => response.json())
+        .then(json => {
+            result = json.data
+            console.log(json.data)
+
+            result?.length ?
+                document.querySelector("#blogcard").innerHTML = result.map((res) =>
+                    `
+      <div class="blog-sect">
+               <img src=${res?.image} alt="blog">
+            <a href="../blogs/blog.html?${res._id}" class="readMore">
+        <h1> ${res?.title}</h1>
+        </a>
+       <p> ${res?.description?.slice(1,300)}</p>
+
+      <div class="emoji1">
+      <p>${res?.comments?.length}</p> 
+      <img src="../images/comment.png" alt="" srcset="">
+         </div>
+          </div>
+      `
+                ).join("") :
+                document.querySelector("#blogcard").innerHTML = `<img style="width:150%;"src="https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif" alt="spinner">`
+        })
+        .catch(err => console.log(err));
+}
+getBlogs();
